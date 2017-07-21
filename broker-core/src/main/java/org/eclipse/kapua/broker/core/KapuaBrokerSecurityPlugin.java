535b4e6a747374f031dc883d7113ff9f6081bd37
stefanomorson <stefano.morson@eurotech.com>
2017-07-21 22:37:13 +0200

    Removed static initialization of shiro
    Signed-off-by: stefanomorson <stefano.morson@eurotech.com>

--- broker-core/src/main/java/org/eclipse/kapua/broker/core/KapuaBrokerSecurityPlugin.java
+++ broker-core/src/main/java/org/eclipse/kapua/broker/core/KapuaBrokerSecurityPlugin.java
@@ -19,18 +19,21 @@ import static org.eclipse.kapua.commons.setting.system.SystemSettingKey.DB_SCHEM
 import static org.eclipse.kapua.commons.setting.system.SystemSettingKey.DB_SCHEMA_ENV;
 import static org.eclipse.kapua.commons.setting.system.SystemSettingKey.DB_USERNAME;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerPlugin;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.config.Ini;
-import org.apache.shiro.config.IniSecurityManagerFactory;
-import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.mgt.AbstractSessionManager;
+import org.apache.shiro.session.mgt.AbstractValidatingSessionManager;
+import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.broker.core.plugin.KapuaSecurityBrokerFilter;
 import org.eclipse.kapua.commons.setting.system.SystemSetting;
+import org.eclipse.kapua.service.authentication.shiro.KapuaAuthenticator;
 import org.eclipse.kapua.service.liquibase.KapuaLiquibaseClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -66,15 +69,52 @@ public class KapuaBrokerSecurityPlugin implements BrokerPlugin {
         new KapuaLiquibaseClient(resolveJdbcUrl(), dbUsername, dbPassword, Optional.of(schema)).update();
 
         try {
-            // initialize shiro context for broker plugin from shiro ini file
-            final URL shiroIniUrl = getClass().getResource("/shiro.ini");
-            Ini shiroIni = new Ini();
-            try (final InputStream input = shiroIniUrl.openStream()) {
-                shiroIni.load(input);
+            // org.apache.shiro.config.Ini
+            // org.apache.shiro.config.IniSecurityManagerFactory
+            // org.apache.shiro.util.Factory;
+            // Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+            // org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+            // SecurityUtils.setSecurityManager(securityManager);
+
+            // Make the SecurityManager instance available to the entire application:
+            Collection<Realm> realms = new ArrayList<>();
+            try {
+                realms.add(new org.eclipse.kapua.service.authentication.shiro.realm.UserPassAuthenticatingRealm());
+                realms.add(new org.eclipse.kapua.service.authorization.shiro.KapuaAuthorizingRealm());
+            } catch (KapuaException e) {
+                // TODO add default realm???
             }
 
-            SecurityManager securityManager = new IniSecurityManagerFactory(shiroIni).getInstance();
-            SecurityUtils.setSecurityManager(securityManager);
+            DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+            defaultSecurityManager.setAuthenticator(new KapuaAuthenticator());
+            logger.info("Set '{}' as shiro authenticator", KapuaAuthenticator.class);
+            // if (defaultSecurityManager.getAuthenticator() instanceof ModularRealmAuthenticator) {
+            // KapuaAuthenticationStrategy authenticationStrategy = new KapuaAuthenticationStrategy();
+            // ((ModularRealmAuthenticator) defaultSecurityManager.getAuthenticator()).setAuthenticationStrategy(authenticationStrategy);
+            // logger.info("Set '{}' as shiro authentication strategy ", KapuaAuthenticationStrategy.class);
+            // }
+            // else {
+            // logger.warn("Cannot set '{}' as shiro authentication strategy! Authenticator class is '{}' and this option is only available for ModularRealmAuthenticator!",
+            // new Object[]{KapuaAuthenticationStrategy.class, defaultSecurityManager.getAuthenticator() != null ? defaultSecurityManager.getAuthenticator().getClass() : "null"});
+            // }
+
+            defaultSecurityManager.setRealms(realms);
+            SecurityUtils.setSecurityManager(defaultSecurityManager);
+
+            // TODO in the old application this code was executed only inside the broker context
+            // but now it's no more needed since we are using an external service (that returns a token) to authenticate the broker component
+            if (defaultSecurityManager.getSessionManager() instanceof AbstractSessionManager) {
+                ((AbstractSessionManager) defaultSecurityManager.getSessionManager()).setGlobalSessionTimeout(-1);
+                logger.info("Shiro global session timeout set to indefinite.");
+            } else {
+                logger.warn("Cannot set Shiro global session timeout to indefinite.");
+            }
+            if (defaultSecurityManager.getSessionManager() instanceof AbstractValidatingSessionManager) {
+                ((AbstractValidatingSessionManager) defaultSecurityManager.getSessionManager()).setSessionValidationSchedulerEnabled(false);
+                logger.info("Shiro global session validator scheduler disabled.");
+            } else {
+                logger.warn("Cannot disable Shiro session validator scheduler.");
+            }
 
             // install the filters
             return new KapuaSecurityBrokerFilter(broker);
