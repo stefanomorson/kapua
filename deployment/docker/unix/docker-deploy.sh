c84682afea2ba64b64a279f9b8444d01fb44b34c
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2019-06-17 16:29:10 +0200

    CertificateService JPA implementation
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- deployment/docker/unix/docker-deploy.sh
+++ deployment/docker/unix/docker-deploy.sh
@@ -15,7 +15,14 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . ${SCRIPT_DIR}/docker-common.sh
 
+if [[ -z ${KAPUA_JWT_CERTIFICATE} ]] || [[ -z ${KAPUA_JWT_PRIVATE_KEY} ]]
+then
+    echo "Either KAPUA_JWT_CERTIFICATE or KAPUA_JWT_PRIVATE_KEY are not set. For more information please refer to the Kapua Documentation."
+    exit 1;
+fi
+
 echo "Deploying Eclipse Kapua..."
+
 if [ -n "${KAPUA_BROKER_DEBUG_PORT}" ]; then
   if [ "${KAPUA_BROKER_DEBUG_SUSPEND}" == "true" ]; then
     KAPUA_BROKER_DEBUG_SUSPEND="y"
@@ -26,5 +33,6 @@ if [ -n "${KAPUA_BROKER_DEBUG_PORT}" ]; then
 else
   docker-compose -f ${SCRIPT_DIR}/../compose/docker-compose.yml up -d
 fi
+
 echo "Deploying Eclipse Kapua... DONE!"
 echo "Run \"docker-compose -f ${SCRIPT_DIR}/../compose/docker-compose.yml logs -f\" for container logs"
